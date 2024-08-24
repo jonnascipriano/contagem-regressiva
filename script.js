da695c6c7d1c72611a01ffb9815d75fbb08f5344
Jonnas <jonnascipriano@gmail.com>
2024-08-24 12:34:06 -0300

    corrigi o display da ul

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
-function contagemRegressiva(){
 
+function contagemRegressiva(){
+    
     const dataEsc = document.getElementById('dataEsc').value
     const dataFinal = new Date(dataEsc).getTime()
     const agora = new Date().getTime()
     const diferenca = dataFinal - agora
-
+    
     const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24))
     const horas = Math.floor((diferenca % (1000*60*60*24)) / (1000*60*60))
     const minutos = Math.floor((diferenca %(1000*60*60))/(1000*60));
     const segundos = Math.floor((diferenca % (1000*60))/1000)
+
 
     document.getElementById('dias').innerText = `${dias}d`
     document.getElementById('hor').innerText = `${horas}h`
@@ -21,6 +23,8 @@ function contagemRegressiva(){
         document.getElementById('res').innerText = 'A contagem terminou'
         document.getElementById('ul').style.display = 'none'
     }
+
+ 
 
     console.log(diferenca)
 }
